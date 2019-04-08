e169b3f8f6e1d284d8cb5d05a615d4f629de3cde
ire-and-curses <eric.saunders@gmail.com>
2019-04-08 16:20:26 -0700

    delete all testnet accounts

--- exp/txnbuild/cmd/demo/demo.go
+++ exp/txnbuild/cmd/demo/demo.go
@@ -16,6 +16,8 @@ import (
 	"github.com/stellar/go/keypair"
 )
 
+const friendbotAddress = "GAIH3ULLFQ4DGSECF2AR555KZ4KNDGEKN4AFI4SU2M7B43MGK3QJZNSR"
+
 func main() {
 	resetF := flag.Bool("reset", false, "Remove all testing state")
 	flag.Parse()
@@ -26,72 +28,145 @@ func main() {
 	if *resetF {
 		fmt.Println("Resetting TestNet state...")
 		reset(client, keys)
-		fmt.Println("Reset complete")
+		fmt.Println("Reset complete.")
 	}
 }
 
 func reset(client *horizonclient.Client, keys []key) {
-	// Check if test0 account exists
-	accountRequest := horizonclient.AccountRequest{AccountId: keys[0].Address}
-	horizonSourceAccount, err := client.AccountDetail(accountRequest)
-	dieIfError(fmt.Sprintf("couldn't get account detail for %s", keys[0].Address), err)
-	sourceAccount := txnbuild.Account{}
-	sourceAccount.FromHorizonAccount(horizonSourceAccount)
-
-	// It exists - so we will proceed to delete it
-	fmt.Println("Found testnet account with ID:", sourceAccount.ID)
-
-	// Find any offers that need deleting...
-	offerRequest := horizonclient.OfferRequest{
-		ForAccount: keys[0].Address,
-		Cursor:     "now",
-		Order:      horizonclient.OrderDesc,
-	}
-	offers, err := client.Offers(offerRequest)
-	dieIfError("error while getting offers", err)
-	fmt.Printf("Account %s has %v offers:\n", keys[0].Address, len(offers.Embedded.Records))
-
-	// ...and delete them
-	for _, o := range offers.Embedded.Records {
-		fmt.Println("    ", o)
-		txe, err := deleteOffer(sourceAccount, uint64(o.ID), keys[0])
-		dieIfError("Problem building deleteOffer op", err)
-		fmt.Printf("Deleting offer %d...\n", o.ID)
-		resp := submit(client, txe)
-		fmt.Println(resp.TransactionSuccessToString())
-		sourceAccount.SequenceNumber++
-	}
-
-	// Find any issued assets for this account...
-	assetRequest := horizonclient.AssetRequest{ForAssetIssuer: keys[0].Address}
-	assets, err := client.Assets(assetRequest)
-	dieIfError(fmt.Sprintf("error while getting issued assets for account %s", keys[0].Address), err)
-	fmt.Printf("Account %s has %v issued assets:\n", keys[0].Address, len(assets.Embedded.Records))
-	for _, a := range assets.Embedded.Records {
-		fmt.Println("    ", a)
-
-		asset := txnbuild.Asset{}
-		hAsset := horizon.Asset{
-			Type:   a.Type,
-			Code:   a.Code,
-			Issuer: a.Issuer,
+	for i, k := range keys {
+		// Check if test0 account exists
+		accountRequest := horizonclient.AccountRequest{AccountId: k.Address}
+		horizonSourceAccount, err := client.AccountDetail(accountRequest)
+		if err != nil {
+			fmt.Printf("    Couldn't get account detail for %s: %s\n", k.Address, err)
+			fmt.Printf("    Skipping further operations on account %s.\n", k.Address)
+			continue
 		}
-		asset.FromHorizonAsset(hAsset)
+		k.Account.FromHorizonAccount(horizonSourceAccount)
+		keys[i].Account.FromHorizonAccount(horizonSourceAccount)
 
-		txe, err := deleteTrustline(sourceAccount, asset, keys[0])
-		dieIfError("Problem building deleteTrustline op", err)
-		fmt.Printf("Deleting trustline %v...\n", asset)
-		resp := submit(client, txe)
-		fmt.Println(resp.TransactionSuccessToString())
-		sourceAccount.SequenceNumber++
+		// It exists - so we will proceed to delete it
+		fmt.Println("\n    Found testnet account with ID:", k.Account.ID)
+
+		// Find any offers that need deleting...
+		offerRequest := horizonclient.OfferRequest{
+			ForAccount: k.Address,
+			Cursor:     "now",
+			Order:      horizonclient.OrderDesc,
+		}
+		offers, err := client.Offers(offerRequest)
+		dieIfError("error while getting offers", err)
+		fmt.Printf("    Account %s has %v offers:\n", k.Address, len(offers.Embedded.Records))
+
+		// ...and delete them
+		for _, o := range offers.Embedded.Records {
+			fmt.Println("    ", o)
+			txe, err := deleteOffer(k.Account, uint64(o.ID), k)
+			dieIfError("Problem building deleteOffer op", err)
+			fmt.Printf("        Deleting offer %d...\n", o.ID)
+			resp := submit(client, txe)
+			fmt.Println(resp.TransactionSuccessToString())
+			k.Account.SequenceNumber++
+		}
+
+		// Find any authorised trustlines on this account...
+		fmt.Printf("    Account %s has %d balances...\n", k.Address, len(horizonSourceAccount.Balances))
+
+		// ...and delete them
+		for _, b := range horizonSourceAccount.Balances {
+			// Native balances don't have trustlines
+			if b.Type == "native" {
+				continue
+			}
+			asset := txnbuild.Asset{}
+			hAsset := horizon.Asset{
+				Type:   b.Type,
+				Code:   b.Code,
+				Issuer: b.Issuer,
+			}
+			asset.FromHorizonAsset(hAsset)
+
+			// Send the asset back to the issuer...
+			fmt.Printf("        Sending %v of surplus asset %s:%s back to issuer...\n", b.Balance, hAsset.Code, hAsset.Issuer)
+			txe, err := payment(k.Account, hAsset.Issuer, b.Balance, asset, k)
+			dieIfError("Problem building payment op", err)
+			resp := submit(client, txe)
+			fmt.Println(resp.TransactionSuccessToString())
+			k.Account.SequenceNumber++
+
+			// Delete the now-empty trustline...
+			fmt.Printf("        Deleting trustline for asset %s:%s...\n", b.Code, b.Issuer)
+			txe, err = deleteTrustline(k.Account, asset, k)
+			dieIfError("Problem building deleteTrustline op", err)
+			resp = submit(client, txe)
+			fmt.Println(resp.TransactionSuccessToString())
+			k.Account.SequenceNumber++
+		}
+
+		// Find any data entries on this account...
+		fmt.Printf("    Account %s has %d data entries...\n", k.Address, len(horizonSourceAccount.Data))
+		for dataKey := range horizonSourceAccount.Data {
+			decodedV, _ := horizonSourceAccount.GetData(dataKey)
+			fmt.Printf("    Deleting data entry '%s' -> '%s'...\n", dataKey, decodedV)
+			txe, err := deleteData(k.Account, dataKey, k)
+			dieIfError("Problem building manageData op", err)
+			resp := submit(client, txe)
+			fmt.Println(resp.TransactionSuccessToString())
+			k.Account.SequenceNumber++
+		}
 	}
 
-	// Merge the account
-	txe, err := mergeAccount(sourceAccount, keys[3], keys[0])
-	dieIfError("Problem building mergeAccount op", err)
+	// Merge the accounts...
+	for _, k := range keys {
+		if k.Account == (txnbuild.Account{}) {
+			continue
+		}
+		fmt.Printf("    Merging account %s back to friendbot (%s)...\n", k.Address, friendbotAddress)
+		txe, err := mergeAccount(k.Account, friendbotAddress, k)
+		dieIfError("Problem building mergeAccount op", err)
+		resp := submit(client, txe)
+		fmt.Println(resp.TransactionSuccessToString())
+	}
+}
 
-	resp := submit(client, txe)
-	fmt.Println(resp.TransactionSuccessToString())
+func deleteData(source txnbuild.Account, k string, signer key) (string, error) {
+	manageData := txnbuild.ManageData{
+		Name: k,
+	}
+
+	tx := txnbuild.Transaction{
+		SourceAccount: source,
+		Operations:    []txnbuild.Operation{&manageData},
+		Network:       network.TestNetworkPassphrase,
+	}
+
+	txeBase64, err := tx.BuildSignEncode(signer.Keypair)
+	if err != nil {
+		return "", errors.Wrap(err, "couldn't serialise transaction")
+	}
+
+	return txeBase64, nil
+}
+
+func payment(source txnbuild.Account, dest, amount string, asset txnbuild.Asset, signer key) (string, error) {
+	payment := txnbuild.Payment{
+		Destination: dest,
+		Amount:      amount,
+		Asset:       &asset,
+	}
+
+	tx := txnbuild.Transaction{
+		SourceAccount: source,
+		Operations:    []txnbuild.Operation{&payment},
+		Network:       network.TestNetworkPassphrase,
+	}
+
+	txeBase64, err := tx.BuildSignEncode(signer.Keypair)
+	if err != nil {
+		return "", errors.Wrap(err, "couldn't serialise transaction")
+	}
+
+	return txeBase64, nil
 }
 
 func deleteTrustline(source txnbuild.Account, asset txnbuild.Asset, signer key) (string, error) {
@@ -107,7 +182,6 @@ func deleteTrustline(source txnbuild.Account, asset txnbuild.Asset, signer key) 
 	if err != nil {
 		return "", errors.Wrap(err, "couldn't serialise transaction")
 	}
-	log.Println("Base 64 TX: ", txeBase64)
 
 	return txeBase64, nil
 }
@@ -125,14 +199,13 @@ func deleteOffer(source txnbuild.Account, offerID uint64, signer key) (string, e
 	if err != nil {
 		return "", errors.Wrap(err, "couldn't serialise transaction")
 	}
-	log.Println("Base 64 TX: ", txeBase64)
 
 	return txeBase64, nil
 }
 
-func mergeAccount(source txnbuild.Account, dest key, signer key) (string, error) {
+func mergeAccount(source txnbuild.Account, destAddress string, signer key) (string, error) {
 	accountMerge := txnbuild.AccountMerge{
-		Destination: dest.Address,
+		Destination: destAddress,
 	}
 
 	tx := txnbuild.Transaction{
@@ -145,7 +218,6 @@ func mergeAccount(source txnbuild.Account, dest key, signer key) (string, error)
 	if err != nil {
 		return "", errors.Wrap(err, "couldn't serialise transaction")
 	}
-	log.Println("Base 64 TX: ", txeBase64)
 
 	return txeBase64, nil
 }
@@ -153,6 +225,7 @@ func mergeAccount(source txnbuild.Account, dest key, signer key) (string, error)
 type key struct {
 	Seed    string
 	Address string
+	Account txnbuild.Account
 	Keypair *keypair.Full
 }
 
